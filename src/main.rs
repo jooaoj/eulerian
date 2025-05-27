94d898c1c0d931af3210d85f21b871f882da1084
Jooa Jaakkola <jaakkola.jooa@gmail.com>
2025-05-27 18:54:15 +0300

    Problem 1 solved

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod natural_sum;
 
 fn main() {
-    let problem1 = natural_sum::natural_sum();
+    let problem1 = natural_sum::natural_sum(10000);
     println!("{:?}", problem1);
 }
--- src/natural_sum.rs
+++ src/natural_sum.rs
@@ -1,4 +1,12 @@
 
-pub fn natural_sum() -> i32 {
-	return 0;
+pub fn natural_sum(target: i32) -> i32 {
+	let mut sum: i32 = 0;
+
+	for i in 0..target {
+		if i % 3 == 0 || i % 5 == 0 {
+			sum += i;
+		}
+	}
+
+	sum
 }
